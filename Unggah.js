73545b428f04de0c9753787c6efbb32b13b23a39
Rynn <fahridwiprast@gmail.com>
2025-03-13 19:26:33 +0700

    Update Unggah.js

--- Unggah.js
+++ Unggah.js
@@ -60,6 +60,14 @@ document.addEventListener('click', function(e) {
         document.getElementById('searchResults').style.display = 'none';
     }
 });
+const { createClient } = supabase;
+
+const supabaseUrl = "https://byseuxjiqggbctokhrec.supabase.co"; // Ganti dengan URL Supabase kamu
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ5c2V1eGppcWdnYmN0b2tocmVjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE0OTcxMzYsImV4cCI6MjA1NzA3MzEzNn0.VyaQMbjhWTJosC-D2ODBcHsXplsKTHFw1nZjBLPWTM8"; // Ganti dengan API Key kamu
+
+window.supabase = createClient(supabaseUrl, supabaseKey);
+
+console.log("Supabase Initialized:", window.supabase);
 
 const fileInput = document.querySelector('input[type="file"]');
 
