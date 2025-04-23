ad8819a4bdb66ee194d642a7d20a05fd1486840b
Rynn <fahridwiprast@gmail.com>
2025-04-23 21:01:30 +0700

    Update Login.js
    code

--- Login.js
+++ Login.js
@@ -1,5 +1,5 @@
-const supabaseUrl = 'https://<project-ref>.supabase.co';
-const supabaseKey = '<your-anon-public-api-key>';
+const supabaseUrl = 'https://byseuxjiqggbctokhrec.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ5c2V1eGppcWdnYmN0b2tocmVjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE0OTcxMzYsImV4cCI6MjA1NzA3MzEzNn0.VyaQMbjhWTJosC-D2ODBcHsXplsKTHFw1nZjBLPWTM8';
 const supabase = supabase.createClient(supabaseUrl, supabaseKey);
 
 async function login() {
